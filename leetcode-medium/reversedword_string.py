02f6fa20277fcc1bfa37d38ef14856dc11a25852
Alyana Imperial <allyimperial@gmail.com>
2025-07-27 20:22:21 -0500

    start leetcode medium - reversed word in string

---
+++ leetcode-medium/reversedword_string.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # reverse the order of the string, get rid of the spaces but have the string reversed
+        # it will always have atleast ONE word
+        
+        reversed_str = ""
+
+        
+        
+        # return reversed string
+        return reversed_str
